eb11119ef3023cb78f576f4418c93c85043ba3f8
AliMDhanani <alidhanani2000@gmail.com>
2022-10-18 20:24:39 +0100

    banner buttons styling

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { baseUrl } from "../constants/movie";
+import { FaPlay } from "react-icons/fa";
+import { HiInformationCircle } from "react-icons/hi";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -27,12 +29,18 @@ const Banner = ({ netflixOriginals }: Props) => {
         />
       </div>
 
-      <h1 className="text-2xl lg:text-7xl md:text-4xl text-white font-bold">{movie?.title || movie?.name || movie?.original_name}</h1>
-      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-white">{movie?.overview}</p>
+      <h1 className="text-2xl lg:text-7xl md:text-4xl text-white font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-white">
+        {movie?.overview}
+      </p>
 
-      <div>
-        <button></button>
-        <button></button>
+      <div className="flex space-x-3">
+        <button className="button__banner bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button className="button__banner bg-[gray]/50"><HiInformationCircle className="h-5 w-5 md:h-8 md:w-8" />More Info</button>
       </div>
     </div>
   );
